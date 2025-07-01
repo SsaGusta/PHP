a4fccb8cc6e7bfe60ee501f64bae38deaafe1165
Gusta <gustavoliveiragf15@gmail.com>
2025-07-01 15:37:24 -0300

    feat: data input

--- Frist Class/Screen_match.php
+++ Frist Class/Screen_match.php
@@ -1,11 +1,16 @@
 <?php
 
-echo "Bem Vindo(a) ao Screen Match!
-";
+echo "Bem Vindo(a) ao Screen Match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
-$anoLancamento = 2022;
+$anoLancamento = $argv[1] ?? 2022;
+// Para solicitar valores do terminal usa argv[1](para pegar somente o primeiro indice)
+
+// ?? siginifa que se o operador da esquerda for nulo(noa for inserido pelo usuário),
+// retornará a resposta que esta a direita
 $notaFilme = 8.8;
 $incluidoNoPlano = true;
 
-echo $nomeFilme;
+echo "Nome do filme: $nomeFilme\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Ano do filme: $anoLancamento\n";
