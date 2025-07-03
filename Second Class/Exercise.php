645178b8a7d0435a87040d56a4148b4b643a214a
Gusta <gustavoliveiragf15@gmail.com>
2025-07-03 14:58:28 -0300

    feat: calculate IMC(BMI)

--- Second Class/Exercise.php
+++ Second Class/Exercise.php
@@ -10,3 +10,24 @@ for($contador = 0; $contador < $numTotal; $contador++){
     }
     echo "$numImpar\n";
 }
+
+echo "\n---------------------------------------------------\n\n";
+
+$altura = $argv[1];
+$peso = $argv[2];
+
+$imc = $peso / ($altura*$altura);
+$resultado = number_format($imc,2);
+
+
+if ($resultado <= 20){
+    echo "Magro. \nIMC: $resultado";
+}elseif($resultado > 20 || $resultado <80){
+    echo "Saudavel. \nIMC $resultado";
+} else{
+    echo "Acima do peso. \nIMC: $resultado";
+}
+
+echo "\n\n---------------------------------------------------\n\n";
+
+
