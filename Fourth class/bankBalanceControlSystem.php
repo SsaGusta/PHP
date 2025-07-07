3e4b05918facbc0a08d653fbe828e184dd64d19d
Gusta <gustavoliveiragf15@gmail.com>
2025-07-07 14:34:39 -0300

    feat: Solution implementation

--- Fourth class/bankBalanceControlSystem.php
+++ Fourth class/bankBalanceControlSystem.php
@@ -1,5 +1,63 @@
 <?php
 
-echo "Digite um numero: \n"
-$numero = fgets(STDIN);
-echo "O numero é: $numero\n";
+$saldo = 1_000;
+$titularConta = 'Gusta Oliver';
+
+echo "******************\n";
+echo "Titular: $titularConta\n";
+echo "Saldo atual: R$ $saldo\n\n";
+echo "******************\n";
+
+do{
+    echo "1. Consultar saldo atual\n";
+    echo "2. Sacar valor\n";
+    echo "3. Depositar valor\n";
+    echo "4. Sair\n";
+    echo "******************\n\n";
+
+    echo "Digite uma opçao: ";
+    $opcao = fgets(STDIN);
+
+    switch ($opcao) {
+        case 1:
+            echo "******************\n";
+            echo "Titular: $titularConta\n";
+            echo "Saldo atual: R$ $saldo\n\n";
+            echo "******************\n";
+            break;
+        case 2:
+            echo "Qual valor deseja sacar?\n";
+            $valorSaque = (float) fgets(STDIN);
+            if ($valorSaque > $saldo){
+                echo "Saldo insuficiente para saque\n";
+                echo "******************\n";
+
+            } else {
+                $saldo = $saldo - $valorSaque;
+                echo "Saque Realizado com Sucesso\n";
+                echo "******************\n";
+
+            }
+            break;
+        case 3:
+            echo "Qual valor deseja depositar?\n";
+            $valorDepo = (float) fgets(STDIN);
+            if($valorDepo > 0){
+                $saldo += $valorDepo;
+                echo "Deposito realizado com sucesso\n";
+                echo "******************\n";
+
+            }else{
+                echo "Insira um valor maior que R$ 0\n";
+                echo "******************\n";
+
+            }
+            break;
+        case 4:
+            echo "Até a proxima!\n";
+            break;
+        default:
+            echo "Opção Inválida\n";
+            break;
+    }
+} while ($opcao !=4);
