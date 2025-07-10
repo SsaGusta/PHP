980edccf39cddeee9f3b9ca9189d5b2e15ad8568
Gusta <gustavoliveiragf15@gmail.com>
2025-07-10 13:25:28 -0300

    feat: defining funcoes directory using DIR

--- Third class/Screen_match.php
+++ Third class/Screen_match.php
@@ -1,20 +1,6 @@
 <?php
 
-function exibeMensagemLancamento(int $ano) : void {
-    if ($ano > 2022){
-    echo "Esse filme é um lançamento\n";
-} elseif($ano > 2020 && $ano <= 2022){
-    echo "Esse filme ainda é novo\n";
-}
-else{
-    echo "Esse filme não é um lançamento";
-}
-}
-
-function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
-    return $planoPrime || $anoLancamento < 2020;
-}
-
+require __DIR__ . "/funcoes.php";
 
 echo "Bem Vindo(a) ao Screen Match!\n";
 
